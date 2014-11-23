e4014e45a5fa14fbab59ef42e6a6364c8a560c2d
Antoni Rasul <antoni.rasul@gmail.com>
2014-11-23 23:49:52 +0100

    Implementing matrix inverse caching for ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+###################################################################
+## Following two functions implement caching of a matrix inverse
+## in order to avoid redundant inverse computation
+###################################################################
+ 
+# Creates a list of functions, used to get/set matrix and its inverse.
+# Represents a matrix and its inverse cache.
+# Args:
+#     mtx: matrix, for which an inverse would be cached
+# Returns:
+#     a list of (set, get, setInverse, getInverse) functions,
+#     used to set/get matrix and its inverse
+makeCacheMatrix <- function(mtx = matrix()) {    
+    inv <- NULL
+    set <- function(pMtx) {
+        mtx <<- pMtx
+        inv <<- NULL
+    }
+    get <- function() {
+        mtx
+    }
+    setInverse <- function(inverseM) {
+        inv <<- inverseM
+    }
+    getInverse <- function() {
+        inv
+    }
+    list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Solves for matrix inverse using the cached data
+# Computes and updates inverse in cache or retrieves from cache.
+# Args:
+#     mtxInvCache: a list of functions, returned by makeCacheMatrix
+# Returns:
+#     inverse of a matrix
+cacheSolve <- function(mtxInvCache, ...) {
+    inv <- mtxInvCache$getInverse()
+    if (!is.null(inv)) {
+        message("Getting matrix inverse from cache")
+        return(inv)
+    }
+    mtx <- mtxInvCache$get()
+    inv <- solve(mtx)
+    mtxInvCache$setInverse(inv)
+    inv
 }
